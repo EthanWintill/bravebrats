86971be1c029dab7e2237b7832f8ebeeb60f6df7
Ethan Wintill <96669967+EthanWintill@users.noreply.github.com>
2022-09-08 01:55:50 -0500

    yummy

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const Game = require('./game');
+var newGame = new Game();
+
 app.use(express.static(__dirname + "/client"));
 app.get('/', (req, res) => {
   
@@ -14,13 +17,17 @@ io.on('connection', (socket) => {
   console.log(socket.id);
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-
+    const choices = msg.split(" ");
+    newGame.chooseCards(parseInt(choices[0]), parseInt(choices[1]));
+    newGame.fight();
   });
 })
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+
 
 
 
